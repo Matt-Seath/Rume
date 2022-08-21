58d0e81ea9c256705601b239339667bf368e3ce8
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-08-21 22:48:31 +1000

    spotify api connected

--- spotify/util.py
+++ spotify/util.py
@@ -1,6 +1,11 @@
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
+from requests import post
+from dotenv import dotenv_values
+
+
+CREDENTIALS = dotenv_values(".env")
 
 
 def get_user_tokens(session_id):
@@ -26,3 +31,34 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
         tokens = SpotifyToken(user=session_id, access_token=access_token,
                               refresh_token=refresh_token, expires_in=expires_in, token_type=token_type)
         tokens.save()
+
+
+def is_spotify_authenticated(session_id):
+    tokens = get_user_tokens(session_id)
+    if tokens:
+        expiry = tokens.expires_in
+        if expiry <= timezone.now():
+            refresh_spotify_token(session_id)
+
+        return True
+
+    return False
+
+
+def refresh_spotify_token(session_id):
+    refresh_token = get_user_tokens(session_id).refresh_token
+
+    response = post("https://accounts.spotify.com/api/token", data={
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+        "client_id": CREDENTIALS["CLIENT_ID"],
+        "client_secret": CREDENTIALS["CLIENT_SECRET"]
+    }).json()
+
+    access_token = response.get("access_token")
+    token_type = response.get("token_type")
+    expires_in = response.get("expires_in")
+    refresh_token = response.get("refresh_token")
+
+    update_or_create_user_tokens(
+        session_id, access_token, token_type, expires_in, refresh_token)
--- spotify/views.py
+++ spotify/views.py
@@ -1,19 +1,18 @@
-from dotenv import dotenv_values
+
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens
-
-CREDENTIALS = dotenv_values(".env")
+from .util import update_or_create_user_tokens, is_spotify_authenticated, CREDENTIALS
 
 
 class AuthURL(APIView):
     def get(self, request, format=None):
+
         scopes = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
 
-        url = Request("GET", "https://accounts.spotify.com/authorize", parama={
+        url = Request("GET", "https://accounts.spotify.com/authorize", params={
             "scope": scopes,
             "response_type": "code",
             "redirect_uri": CREDENTIALS["REDIRECT_URI"],
@@ -27,7 +26,7 @@ def spotify_callback(request, format=None):
     code = request.GET.get('code')
     error = request.GET.get('error')
 
-    response = post("https//accounts.spotify.com/api/token", data={
+    response = post("https://accounts.spotify.com/api/token", data={
         "grant_type": "authorization_code",
         "code": code,
         "redirect_uri": CREDENTIALS["REDIRECT_URI"],
@@ -48,3 +47,10 @@ def spotify_callback(request, format=None):
         request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
     return redirect('frontend:')
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(
+            self.request.session.session_key)
+        return Response({"status": is_authenticated}, status=status.HTTP_200_OK)
