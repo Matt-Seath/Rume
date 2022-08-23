105af23d9bc8530499db6454faf95b8a086a6faf
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-08-24 08:45:41 +1000

    info page added

---
+++ frontend/src/components/Info.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Grid, Button, Typography, IconButton } from "@material-ui/core";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import { Link } from "react-router-dom";
+
+const pages = {
+  JOIN: "pages.join",
+  CREATE: "pages.create",
+};
+
+export default function Info(props) {
+  const [page, setPage] = useState(pages.JOIN);
+
+  function joinInfo() {
+    return "Join an existing room created by another host";
+  }
+
+  function createInfo() {
+    return "Create a room by connecting to a Spotify account";
+  }
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          What is RUME?
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="body1">
+          {page === pages.JOIN ? joinInfo() : createInfo()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <IconButton
+          onClick={() => {
+            page === pages.CREATE ? setPage(pages.JOIN) : setPage(pages.CREATE);
+          }}
+        >
+          {page === pages.CREATE ? (
+            <NavigateBeforeIcon />
+          ) : (
+            <NavigateNextIcon />
+          )}
+        </IconButton>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button color="secondary" variant="contained" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
